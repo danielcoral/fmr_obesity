1c10b7d41b7823b0062d1e77929f74d10c6182fd
danielcoral <daniel.coral@med.lu.se>
2020-07-01 17:20:03 +0200

    Organizing files

--- ukb/codes/fmr_geno.sh
+++ ukb/codes/fmr_geno.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mapfile -t myarray < <(ls /ludc/Active_Projects/Discordant_variant_analysis/Private/bmi_t2d_ukbb/snps | cat | awk '{gsub("grs_chr|.txt",""); print}')
+mapfile -t myarray < <(ls /ludc/Active_Projects/Discordant_variant_analysis/Private/fmr_obesity/data/grs_snps/ | cat | awk '{gsub("snps_chr|.txt",""); print}')
 
-for chr in "${myarray[@]}";do /ludc/Tools/Software/Plink/v2.00a1LM/plink2 --memory 8000 --bgen /ludc/Raw_Data_Archive/UKBB/imp/ukb_imp_chr${chr}_v3.bgen --sample /ludc/Active_Projects/UKBB_18274/Private/ukb18274_imp_chr1_v3_s487314.sample --extract /ludc/Active_Projects/Discordant_variant_analysis/Private/bmi_t2d_ukbb/snps/grs_chr${chr}.txt --make-pgen --out /ludc/Active_Projects/Discordant_variant_analysis/Private/bmi_t2d_ukbb/ukb_genotype/snpcount_chr$chr; done
+for chr in "${myarray[@]}";do /ludc/Tools/Software/Plink/v2.00a1LM/plink2 --memory 8000 --bgen /ludc/Raw_Data_Archive/UKBB/imp/ukb_imp_chr${chr}_v3.bgen --sample /ludc/Active_Projects/UKBB_18274/Private/ukb18274_imp_chr1_v3_s487314.sample --extract /ludc/Active_Projects/Discordant_variant_analysis/Private/fmr_obesity/data/grs_snps/snps_chr${chr}.txt --make-pgen --out /ludc/Active_Projects/Discordant_variant_analysis/Private/fmr_obesity/ukb/a_count/snpcount_chr$chr; done
 
 
